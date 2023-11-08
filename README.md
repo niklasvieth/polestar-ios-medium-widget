2bc3095410271debeeb12622bee7eaf3dda1a497
niklasvieth <niklasvieth@web.de>
2023-11-08 10:56:24 +0100

    feat: add polestar medium SoC widget

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# polestar-ios-medium-widget
-Polestar iOS medium widget via Scriptable
+# Polestar SoC iOS Medium Widget
+
+An iOS medium widget to display the current state of charge (SoC) of your Polestar 2.
+
+<img src="./images/polestar_medium_widget_relative.jpeg" width="300" alt="Polestar Medium Widget Relative Date"/>
+
+<img src="./images/polestar_medium_widget.jpeg" width="300" alt="Polestar Medium Widget"/>
+
+<img src="./images/polestar_medium_widget_relative_low.jpeg" width="300" alt="Polestar Medium Widget Relative Date Low"/>
+
+## Background
+
+Since the official Polestar App does not support any kind of widgets for iOS, I developed my own with the help of [Scriptable](https://scriptable.app/). Without an official public Polestar API being released yet, the widget relies on the [Tibber GraphQL API](https://developer.tibber.com/docs/overview) as proxy to get the current state of charge of the battery. Thus, you will also need Tibber Account to get access to the SoC data.
+
+## Prerequisites
+
+1. Free Tibber Account with configured Polestar Power-Up
+    > :warning: You **don't** need an electricity contract, you can just proceed with the free account.
+    - Install [Tibber App](https://apps.apple.com/de/app/tibber-%C3%B6kostrom/id1127805969) on your iPhone and follow the registration instructions. Make sure to remember the email & password used to create the Tibber account.
+    - [Configure Polestar Power-Up](https://support.tibber.com/en/articles/6675026-smart-charge-your-polestar-with-tibber#:~:text=How%20do%20I%20connect%20my%20Polestar%20to%20Tibber%3F)
+2. Install [Scriptable](https://apps.apple.com/de/app/scriptable/id1405459188) on your iPhone
+3. [Optional but recommended] Follow the [ScriptDude installation steps](https://scriptdu.de/#installation)
+
+## Installation
+
+1. Make sure you have followed all mandatory steps described in [Prerequisites](./README.md#Prerequisites).
+
+2. There are two options to install the widget:
+    - [Recommended] Click on the following button to install via `ScriptDude` (make sure that you have followed step 3 of [Prerequisites](./README.md#Prerequisites)). ScriptDude will automatically receive updates of new versions of this widget
+        [![Download with ScriptDude](https://scriptdu.de/download.svg)](https://scriptdu.de/?name=Polestar+Medium+SoC+Widget&source=https%3A%2F%2Fgist.githubusercontent.com%2Fniklasvieth%2F159c13dd7ef94bd608358ce964b66c7c%2Fraw%2Fe05830e0f1b572089b99e73344ac192e8d7f808d%2Fpolestar-medium-widget.js&docs=https%3A%2F%2Fgithub.com%2Fniklasvieth%2Fpolestar-ios-medium-widget%2Fblob%2Fmain%2FREADME.md#generator)
+
+    - Copy the content of [polestar-medium-widget.js](https://gist.github.com/niklasvieth/159c13dd7ef94bd608358ce964b66c7c), create a new script in `Scriptable`, paste the content and rename it to `Polestar Medium SoC Widget`.
+
+3. Replace the placeholder values for `TIBBER_EMAIL` and `TIBBER_PASSWORD` with your Tibber login credentials.
+
+    ```js
+    // Config
+    const TIBBER_EMAIL = "<EMAIL_ADDRESS>";
+    const TIBBER_PASSWORD = "<PASSWORD>";
+    ```
+
+4. [Optional] You can decide if you prefer the relative live counter or the absolute timestamp for the last seen date in the widget footer. Default is the relative format. Change the value to `false` if you prefer the absolute date.
+
+    ```js
+    const LAST_SEEN_RELATIVE_DATE = true; // false
+    ```
+
+5. Add the medium `Scriptable` widget to your homescreen. See [Apple How-To guide](https://support.apple.com/en-us/HT207122#:~:text=How%20to%20add%20widgets%20to%20your%20Home%20Screen).
+
+    <img src="./images/scriptable_medium_widget.jpeg" width="200" alt="Scriptable Medium Widget"/>
+
+6. Tab on the widget to configure the widget. Select `Polestar Medium SoC Widget` as script, leave everything else blank.
+
+7. You should now be able to see the :battery: SoC of your Polestar on you homescreen. By clicking on the widget you will be navigated to the Polestar App.
+
+## Additional comments
+
+There might be a minor lag or difference of the SoC compared to value displayed in the Polestar App because of the following reasons:
+
+- The widget is relying on the Tibber API which syncs regularly with the actual Polestar API.
+- The refresh interval of the widget is determined by iOS itself.
+
+### Appreciation
+
+- Thanks @simonbs for the awesome [Scriptable](https://scriptable.app/) app.
+
+### Support me
+
+<a href="https://www.paypal.me/niklasvieth" >
+  <img src="https://raw.githubusercontent.com/stefan-niedermann/paypal-donate-button/master/paypal-donate-button.png" alt="Donate with PayPal" width="200px" />
+</a>
---
+++ polestar-medium-widget.js
@@ -0,0 +1,189 @@
+// icon-color: green; icon-glyph: battery-half;
+
+/**
+ * This widget has been developed by Niklas Vieth.
+ * Installation and configuration details can be found at https://github.com/niklasvieth/polestar-ios-medium-widget
+ */
+
+// Config
+const TIBBER_EMAIL = "EMAIL_ADDRESS";
+const TIBBER_PASSWORD = "PASSWORD";
+const LAST_SEEN_RELATIVE_DATE = true; // false
+
+const TIBBER_BASE_URL = "https://app.tibber.com";
+const POLESTAR_ICON = "https://www.polestar.com/w3-assets/coast-228x228.png";
+
+// Check that params are set
+if (TIBBER_EMAIL === "EMAIL_ADDRESS") {
+  throw new Error("Parameter TIBBER_EMAIL is not configured");
+}
+if (TIBBER_PASSWORD === "PASSWORD") {
+  throw new Error("Parameter TIBBER_PASSWORD is not configured");
+}
+
+// You can run the script in the app to preview the widget or you can go to the Home Screen, add a new Scriptable widget and configure the widget to run this script.
+// You can also try creating a shortcut that runs this script. Running the shortcut will show widget.
+const tibberData = await fetchTibberData();
+const widget = await createPolestarWidget(tibberData);
+
+if (config.runsInWidget) {
+  // The script runs inside a widget, so we pass our instance of ListWidget to be shown inside the widget on the Home Screen.
+  Script.setWidget(widget);
+} else {
+  // The script runs inside the app, so we preview the widget.
+  widget.presentMedium();
+}
+Script.complete();
+
+// Create polestar widget
+async function createPolestarWidget(tibberData) {
+  const appIcon = await loadImage(POLESTAR_ICON);
+  const title = tibberData.name;
+  const widget = new ListWidget();
+  widget.url = "polestar-explore://";
+
+  // Add background gradient
+  const gradient = new LinearGradient();
+  gradient.locations = [0, 1];
+  gradient.colors = [new Color("141414"), new Color("13233F")];
+  widget.backgroundGradient = gradient;
+
+  // Show app icon and title
+  const titleStack = widget.addStack();
+  const titleElement = titleStack.addText(title);
+  titleElement.textColor = Color.white();
+  titleElement.textOpacity = 0.7;
+  titleElement.font = Font.mediumSystemFont(14);
+  titleStack.addSpacer();
+  const appIconElement = titleStack.addImage(appIcon);
+  appIconElement.imageSize = new Size(30, 30);
+  appIconElement.cornerRadius = 4;
+  widget.addSpacer(12);
+
+  // Center Stack
+  const contentStack = widget.addStack();
+  const carImage = await loadImage(tibberData.imgUrl);
+  const carImageElement = contentStack.addImage(carImage);
+  carImageElement.imageSize = new Size(150, 100);
+  contentStack.addSpacer();
+
+  // Battery Info
+  const batteryInfoStack = contentStack.addStack();
+  batteryInfoStack.layoutVertically();
+  batteryInfoStack.addSpacer();
+
+  // Battery Percent Value
+  const batteryPercent = tibberData.battery.percent;
+  const isCharging = tibberData.battery.isCharging;
+  const batteryPercentStack = batteryInfoStack.addStack();
+  batteryPercentStack.addSpacer();
+  batteryPercentStack.centerAlignContent();
+  const batterySymbol = getBatteryPercentIcon(batteryPercent, isCharging);
+  const batterySymbolElement = batteryPercentStack.addImage(
+    batterySymbol.image
+  );
+  batterySymbolElement.imageSize = new Size(40, 40);
+  batterySymbolElement.tintColor = getBatteryPercentColor(batteryPercent);
+  batteryPercentStack.addSpacer(8);
+
+  const batteryPercentText = batteryPercentStack.addText(`${batteryPercent} %`);
+  batteryPercentText.textColor = getBatteryPercentColor(batteryPercent);
+  batteryPercentText.font = Font.boldSystemFont(24);
+
+  // Footer
+  const footerStack = batteryInfoStack.addStack();
+  footerStack.addSpacer();
+
+  // Add last seen indicator
+  const lastSeenDate = new Date(tibberData.lastSeen);
+  const lastSeenText = lastSeenDate.toLocaleString();
+  let lastSeenElement;
+  if (LAST_SEEN_RELATIVE_DATE) {
+    lastSeenElement = footerStack.addDate(lastSeenDate);
+    lastSeenElement.applyRelativeStyle();
+  } else {
+    lastSeenElement = footerStack.addText(lastSeenText);
+  }
+  lastSeenElement.font = Font.mediumSystemFont(10);
+  lastSeenElement.textOpacity = 0.5;
+  lastSeenElement.minimumScaleFactor = 0.5;
+  lastSeenElement.rightAlignText();
+
+  return widget;
+}
+
+/********************
+ * Tibber API helpers
+ ********************/
+async function fetchTibberToken() {
+  const tokenUrl = `${TIBBER_BASE_URL}/login.credentials`;
+  const body = {
+    "@type": "login",
+    email: TIBBER_EMAIL,
+    password: TIBBER_PASSWORD,
+  };
+  const req = new Request(tokenUrl);
+  req.method = "POST";
+  req.body = JSON.stringify(body);
+  req.headers = {
+    "Content-Type": "application/json",
+    charset: "utf-8",
+  };
+  const response = await req.loadJSON();
+  return response.token;
+}
+
+async function fetchTibberData() {
+  const tibberToken = await fetchTibberToken();
+  const url = `${TIBBER_BASE_URL}/v4/gql`;
+  const body = {
+    query:
+      "{me{homes{electricVehicles{lastSeen imgUrl name shortName battery{percent chargeLimit isCharging percentColor}}}}}",
+  };
+  const req = new Request(url);
+  req.method = "POST";
+  req.body = JSON.stringify(body);
+  req.headers = {
+    "Content-Type": "application/json",
+    charset: "utf-8",
+    Authorization: `Bearer ${tibberToken}`,
+  };
+  const response = await req.loadJSON();
+  return response.data.me.homes[0].electricVehicles[0];
+}
+
+async function loadImage(url) {
+  const req = new Request(url);
+  return req.loadImage();
+}
+
+/*************
+ * Formatters
+ *************/
+function getBatteryPercentColor(percent) {
+  if (percent > 60) {
+    return Color.green();
+  } else if (percent > 30) {
+    return Color.orange();
+  } else {
+    return Color.red();
+  }
+}
+
+function getBatteryPercentIcon(percent, isCharging) {
+  if (isCharging) {
+    return SFSymbol.named(`battery.100percent.bolt`);
+  }
+  let percentRounded = 0;
+
+  if (percent > 90) {
+    percentRounded = 100;
+  } else if (percent > 60) {
+    percentRounded = 75;
+  } else if (percent > 40) {
+    percentRounded = 50;
+  } else if (percent > 10) {
+    percentRounded = 25;
+  }
+  return SFSymbol.named(`battery.${percentRounded}`);
+}
